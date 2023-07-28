a176d78895cb79162098d1ec61d59bb84d252663
kcn3388 <kcn3388@126.com>
2023-07-28 15:45:40 +0800

    update to latest

--- waifu-tips.js
+++ waifu-tips.js
@@ -12,6 +12,23 @@
 
     let t;
 
+    function showMessage(text, timeout, priority) {
+        if (!text || (sessionStorage.getItem("waifu-text") && sessionStorage.getItem("waifu-text") > priority)) return;
+        if (messageTimer) {
+            clearTimeout(messageTimer);
+            messageTimer = null;
+        }
+        text = randomSelection(text);
+        sessionStorage.setItem("waifu-text", priority);
+        const tips = document.getElementById("waifu-tips");
+        tips.innerHTML = text;
+        tips.classList.add("waifu-tips-active");
+        messageTimer = setTimeout(() => {
+            sessionStorage.removeItem("waifu-text");
+            tips.classList.remove("waifu-tips-active");
+        }, timeout);
+    }
+
     function o(o, s, n) {
         if (!o || sessionStorage.getItem("waifu-text") && sessionStorage.getItem("waifu-text") > n) return;
         t && (clearTimeout(t), t = null), o = e(o), sessionStorage.setItem("waifu-text", n);
